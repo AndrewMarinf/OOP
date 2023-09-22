bfd40debaa92c38f79fba245c15484705f17e719
AndrewMarinf <andrew.marin@bk.ru>
2023-09-22 22:52:45 +0300

    property - password

--- file_11.py
+++ file_11.py
@@ -78,10 +78,10 @@
 
 # Access.get_access(5) # печатает AccessTypeError
 
-class User:
-    def __init__(self,name,role) -> None:
-        self.name = name
-        self.role = role
+# class User:
+#     def __init__(self,name,role) -> None:
+#         self.name = name
+#         self.role = role
 
 # class Access:
 #     __access_list = ['admin','developer']
@@ -126,13 +126,84 @@ class User:
 # логин должен содержать символ точки «.» после символа «@».В случае, если после @ нету точки, вызываем исключение при помощи строки raise ValueError
 # Если значение проходит проверку новое значение логина сохраняется в атрибут self.__login
 # Напишите определение класса Registration       
+# class Registration:
+#     def __init__(self,login,) -> str:
+#         self.login = login
+
+#     @property
+#     def login(self):
+#         return self.__login
+
+#     @login.setter
+#     def login(self,value):
+#         if not isinstance(value,str):
+#             raise TypeError
+#         elif  not "@" in value:
+#             raise ValueError
+#         elif not value.count('@') <=1:
+#             raise ValueError      
+#         elif not '.' in value[value.find('@'):]:
+#             raise ValueError
+            
+#         self.__login = value
+
+# # Ниже код для проверки класса Registration
+
+
+# try:
+#     result = Registration("fga")
+# except ValueError as error:
+#     print("Логин fga должен содержать один символ '@'")
+
+# try:
+#     result = Registration(1234)
+# except TypeError as error:
+#     print("Пароль должен быть строкой")
+
+# try:
+#     result = Registration("f@ga@")
+# except ValueError as error:
+#     print("Логин f@ga@ должен содержать только один символ '@'")
+
+# try:
+#     result = Registration("fg@a")
+# except ValueError as error:
+#     print("В логине fg@a должен быть символ '.' после символа '@'")
+
+# try:
+#     result = Registration("fg.@a")
+# except ValueError as error:
+#     print("В логине fg.@a должна быть '.' после символа '@'")
+
+# result = Registration("translate@gmail.com")
+# assert result.login == "translate@gmail.com"
+# assert result._Registration__login == "translate@gmail.com"
+
+# try:
+#     result.login = "asdsa12asd."
+# except ValueError as error:
+#     print("Логин asdsa12asd. должен содержать один символ '@'")
+
+# try:
+#     result.login = "asdsa12@asd"
+# except ValueError as error:
+#     print("asdsa12@asd должен быть символ '.' после символа '@'")
+
+# result.login = "alligator13@how.do"
+# assert result.login == "alligator13@how.do"
+# assert result._Registration__login == "alligator13@how.do"
+        
+
+
 class Registration:
-    def __init__(self,login,) -> str:
+    def __init__(self,login,password) -> str:
         self.login = login
+        self.password = password
 
     @property
     def login(self):
         return self.__login
+    @property
 
     @login.setter
     def login(self,value):
@@ -146,51 +217,3 @@ class Registration:
             raise ValueError
             
         self.__login = value
-
-# Ниже код для проверки класса Registration
-
-
-try:
-    result = Registration("fga")
-except ValueError as error:
-    print("Логин fga должен содержать один символ '@'")
-
-try:
-    result = Registration(1234)
-except TypeError as error:
-    print("Пароль должен быть строкой")
-
-try:
-    result = Registration("f@ga@")
-except ValueError as error:
-    print("Логин f@ga@ должен содержать только один символ '@'")
-
-try:
-    result = Registration("fg@a")
-except ValueError as error:
-    print("В логине fg@a должен быть символ '.' после символа '@'")
-
-try:
-    result = Registration("fg.@a")
-except ValueError as error:
-    print("В логине fg.@a должна быть '.' после символа '@'")
-
-result = Registration("translate@gmail.com")
-assert result.login == "translate@gmail.com"
-assert result._Registration__login == "translate@gmail.com"
-
-try:
-    result.login = "asdsa12asd."
-except ValueError as error:
-    print("Логин asdsa12asd. должен содержать один символ '@'")
-
-try:
-    result.login = "asdsa12@asd"
-except ValueError as error:
-    print("asdsa12@asd должен быть символ '.' после символа '@'")
-
-result.login = "alligator13@how.do"
-assert result.login == "alligator13@how.do"
-assert result._Registration__login == "alligator13@how.do"
-        
-            
