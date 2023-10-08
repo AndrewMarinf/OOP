dbc1f41643e84339ce2167ad2ec84c6a50d8aaf1
AndrewMarinf <andrew.marin@bk.ru>
2023-10-08 21:50:24 +0300

    наследие

--- file_4.3.py
+++ file_4.3.py
@@ -24,3 +24,41 @@ assert power4.get_value(5) == 625
 assert power4.get_value(25) == 390625
 
 print('Good')
+
+
+
+class CURD:
+    def __init__(self):
+        C().create()
+        R().read()
+        U().update()
+        D().delete()
+
+
+class C:
+    def create(self):
+        print("c", end='')
+
+
+class U:
+    def update(self):
+        print("u", end='')
+
+
+class R(C):
+    def create(self):
+        print("C", end='')
+
+    def read(self):
+        print("R", end='')
+
+
+class D(U):
+    def update(self):
+        print("U", end='')
+
+    def delete(self):
+        print("D", end='')
+
+
+CURD()
