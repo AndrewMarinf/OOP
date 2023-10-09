b440ebeed95068a403c32122605dee6a56d29904
AndrewMarinf <andrew.marin@bk.ru>
2023-10-09 11:09:58 +0300

    Делегирование1_

---
+++ file_4.5.py
@@ -0,0 +1,25 @@
+
+class Person:
+    def __init__(self,name,passport) -> str:
+        self.name = name
+        self.passport = passport
+
+    def display(self):
+        print(f'{self.name}: {self.passport}')  
+
+class Employee(Person):
+    def __init__(self, name, passport,salary,department) -> str:
+        super().__init__(name, passport)
+        self.salary = salary
+        self.department = department
+
+assert issubclass(Employee, Person)
+
+emp = Person("just human", 123456)
+emp.display()
+assert emp.__dict__ == {'name': 'just human', 'passport': 123456}
+
+emp2 = Employee("Geek2", 534432, 321321, 'Roga & Koputa')
+emp2.display()
+assert emp2.__dict__ == {'salary': 321321, 'department': 'Roga & Koputa',
+                         'name': 'Geek2', 'passport': 534432}
