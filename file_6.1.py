4a92a100b6cab5c5835fc1fa4bcf89a72c9f89d1
AndrewMarinf <andrew.marin@bk.ru>
2023-10-21 22:13:14 +0300

    добавил задачи

--- file_6.1.py
+++ file_6.1.py
@@ -1,26 +1,44 @@
-# from dataclasses import dataclass
+# Попробуйте создать свой первый dataclass
+# Это будет простой класс Point, который должен хранить два целых атрибута x и y .
+# На основании класса Point создайте
+#  точку с координатами (5, 7) и сохраните ее в  переменную point1
+# точку с координатами (-10, 12) и сохраните ее в  переменную point2
+# Выведите сперва point1 потом на отдельной строке point2
 
-# @dataclass
-# class Point:
-#     x: int
-#     y: int
+from dataclasses import dataclass
 
-# point1 = Point(5, 7)
-# point2 = Point(-10, 12)
+@dataclass
+class Point:
+    x: int
+    y: int
 
-# print(point1)
-# print(point2)
+point1 = Point(5, 7)
+point2 = Point(-10, 12)
+
+print(point1)
+print(point2)
 
 
-# from dataclasses import dataclass
 
-# @dataclass
-# class Location:
-#     name: str
-#     longitude: int = 0
-#     latitude: int = 11.5
+# Создайте датакласс  Location
 
-# stronehenge = Location( name='Stonehenge', longitude=51, latitude=1.5)
+# В нем должны быть описаны следующие атрибуты
+
+# name - обязательный, тип строка
+# longitude - необязательный, вещественный тип, значение по умолчанию 0
+# latitude - необязательный, вещественный тип, значение по умолчанию 11.5
+# Создайте ЭК Location со значениями name='Stonehenge', longitude=51, latitude=1.5  и сохраните его в переменную stonehenge 
+
+
+from dataclasses import dataclass
+
+@dataclass
+class Location:
+    name: str
+    longitude: int = 0
+    latitude: int = 11.5
+
+stronehenge = Location( name='Stonehenge', longitude=51, latitude=1.5)
 
 
 class Person:
