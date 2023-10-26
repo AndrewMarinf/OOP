c767c802882bf98be89ce9592db444582c60541e
AndrewMarinf <andrew.marin@bk.ru>
2023-10-26 10:07:02 +0300

    new_26

--- everything_in_a_row.py
+++ everything_in_a_row.py
@@ -169,7 +169,7 @@ assert d.peek() == 'hello'
 
 
 
-
+# ====================================================================================================
 
 persons= [
     ('Allison Hill', 334053, 'M', '1635644202'),
@@ -215,4 +215,71 @@ class CustomLabel:
 
     def config(self,**kw):
         self.__dict__.update(kw)
-        
+# ===========================================================================================================================================       
+
+class Person:
+
+    def __init__(self,name,age) -> None:
+        self.name = name
+        self.age = age
+
+    def display_person_info(self):
+        print(f'Person: {self.name}, {self.age}')
+
+
+class Company:
+
+    def __init__(self,company_name,location) -> None:
+        self.company_name = company_name
+        self.location = location
+
+    def display_company_info(self):
+        print(f'Company: {self.company_name}, {self.location}')  
+
+class Employee:
+
+    def __init__(self, name, age, company_name, location):
+        self.personal_data = Person(name, age)
+        self.work = Company(company_name, location)
+
+
+# ===================================================================================================
+#  не решил 2.3 задача
+class Task:
+    def __init__(self,name,description,status) -> str:
+        self.name = name
+        self.description = description
+        self.status = status
+
+    def display(self):
+        print(f'​​​​​{self.name} {self.status}')
+
+
+class TaskList:
+    
+    def __init__(self) -> str:
+        self.tasks = []
+
+    def add_task(self,a):
+        self.tasks.append(a)
+
+    def remove_task(self,b):
+        self.tasks.remove(b)
+
+class TaskManager:
+    def __init__(self,) -> None:
+        pass
+
+# ==============================================================================================
+# 
+#                 
+
+
+
+
+
+
+
+
+
+
